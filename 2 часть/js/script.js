b5a926f69190272e904c08e62aa25c96b0fd65e2
Vitalik <vit198837@mail.ru>
2019-11-29 15:10:10 +0300

    таймер + доп,задание

--- 2 часть/js/script.js
+++ 2 часть/js/script.js
@@ -40,4 +40,50 @@ window.addEventListener('DOMContentLoaded', function () {
         };
     });
 
+
+    // Time
+
+    let deadline = '2020-01-01';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+        if (10 / seconds > 1) {
+            seconds = ('0' + seconds);
+        };
+        if (10 / minutes > 1) {
+            minutes = ('0' + minutes);
+        };
+        return {
+            'total': t,
+            'seconds': seconds,
+            'minutes': minutes,
+            'hours': hours
+        };
+    };
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            };
+        };
+
+    };
+
+    setClock('timer', deadline);
+
 });
