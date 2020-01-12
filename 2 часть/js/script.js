1d6fc5f0a4f3cc62598ba8084f2b6cb428d36f36
Vitalik <vit198837@mail.ru>
2020-01-12 19:39:03 +0300

    дописал слайдер

--- 2 часть/js/script.js
+++ 2 часть/js/script.js
@@ -168,13 +168,50 @@ window.addEventListener('DOMContentLoaded', function () {
     });
 
 
-//   Слайдер
+    //   Слайдер
 
-let sliderIndex = 1,
-slides = document.querySelectorAll('.slider-item'),
-prev = document.querySelector('.prev'),
-next = document.querySelector('.next'),
-dotsWrap = document.querySelector('.slider-dots'),
-dot = document.querySelectorAll('.dot');
+    let sliderIndex = 1,
+        slides = document.querySelectorAll('.slider-item'),
+        prev = document.querySelector('.prev'),
+        next = document.querySelector('.next'),
+        dotsWrap = document.querySelector('.slider-dots'),
+        dots = document.querySelectorAll('.dot');
+
+    function showSlides(n) {
+
+        if (n > slides.length) {
+            sliderIndex = 1;
+        }
+        if (n < 1) {
+            sliderIndex = slides.length;
+        }
+
+        slides.forEach((item) => item.style.display = 'none');
+        dots.forEach((item) => item.classList.remove('dot-active'));
+        slides[sliderIndex - 1].style.display = 'block';
+        dots[sliderIndex - 1].classList.add('dot-active');
+    };
+
+    showSlides(sliderIndex);
+
+    function plusSlides(n) {
+        showSlides(sliderIndex += n);
+    };
+    function currentSlides(n) {
+        showSlides(sliderIndex = n);
+    };
+    prev.addEventListener('click', () => {
+        plusSlides(-1);
+    });
+    next.addEventListener('click', () => {
+        plusSlides(1);
+    });
+    dotsWrap.addEventListener('click', (e) => {
+        for (let i = 0; i < dots.length + 1; i++) {
+            if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+                currentSlides(i);
+            }
+        }
+    });
 
 });
